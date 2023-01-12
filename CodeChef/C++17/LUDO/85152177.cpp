72e8988da94c52122525db3070ef9dd77aa17203
Hetan Nandre <97376783+nandrehetan@users.noreply.github.com>
2023-01-12 23:23:33 +0530

    Committing files

---
+++ CodeChef/C++17/LUDO/85152177.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n>=6)  cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+	// your code goes here
+	return 0;
+}
