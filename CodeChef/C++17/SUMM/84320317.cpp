d893fe4231a13a212f34a6c2d03e30f65e6b58d9
Hetan Nandre <97376783+nandrehetan@users.noreply.github.com>
2023-01-12 23:23:55 +0530

    Committing files

---
+++ CodeChef/C++17/SUMM/84320317.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        
+    
+    int a,b,c;
+    cin>>a>>b>>c;
+    if((a+b)==c) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+	// your code goes here
+	
+    }
+    return 0;
+}
